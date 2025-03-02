7f69d5369c68cfdb0f287ae3c70b42a1848cffad
eheidl <erhard.heidl@4players.de>
2025-03-02 01:55:16 +0100

    APK_1_0 fix

--- lib/ui/screens/settings_screen.dart
+++ lib/ui/screens/settings_screen.dart
@@ -299,7 +299,7 @@ class SettingsScreen extends StatelessWidget {
             const SizedBox(height: 16),
             const Text(
               '''Wetterdaten werden über die OpenWeatherMap API bereitgestellt.
-              Scripted by Ghoses 2025 - www.ape-x.net // www.github.com/ghoses''',
+Scripted by Ghoses 2025 - www.ape-x.net // www.github.com/ghoses''',
               style: TextStyle(fontSize: 12, color: Colors.grey),
             ),
           ],
